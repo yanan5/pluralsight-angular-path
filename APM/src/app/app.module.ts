37912b798b94742be7a0cc6f437380e00a42b7b3
Ramaswamy Narayanan <ramaswamy.bn@perficient.com>
2020-04-20 15:36:31 +0530

    Added Product list comp,currency pipe, uppercase pipe, ngModel biding for filter text

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsListComponent } from './products-list/products-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsListComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ APM/src/app/products-list/products-list.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products-list',
+  templateUrl: './products-list.component.html',
+  styleUrls: ['./products-list.component.scss']
+})
+export class ProductsListComponent implements OnInit {
+  pageTitle: string = "Product List";
+  imageWidth: number = 50;
+  imageHeight: number = 50;
+  imageMargin: number = 2;
+  products: any[] = [{
+    "productId": 1,
+    "productName": "Leaf Rake",
+    "productCode": "GDN-0011",
+    "releaseDate": "March 19, 2019",
+    "description": "Leaf rake with 48-inch wooden handle.",
+    "price": 19.95,
+    "starRating": 3.2,
+    "imageUrl": "assets/images/leaf_rake.png"
+  },
+  {
+    "productId": 2,
+    "productName": "Garden Cart",
+    "productCode": "GDN-0023",
+    "releaseDate": "March 18, 2019",
+    "description": "15 gallon capacity rolling garden cart",
+    "price": 32.99,
+    "starRating": 4.2,
+    "imageUrl": "assets/images/garden_cart.png"
+  }];
+  showImage: boolean = false;
+  filterText: string;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  toggleImage() {
+    this.showImage = !this.showImage
+  }
+
+}
