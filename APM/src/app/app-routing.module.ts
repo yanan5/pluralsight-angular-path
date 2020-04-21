aa18c4f07485badf199ed1d1a66a463efbf14672
Ramaswamy Narayanan <ramaswamy.bn@perficient.com>
2020-04-21 17:47:41 +0530

    added canActivate route guard

--- APM/src/app/app-routing.module.ts
+++ APM/src/app/app-routing.module.ts
@@ -3,12 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { ProductsListComponent } from './products-list/products-list.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-
+import { ProductDetailGuard } from './guards/product-detail/product-detail.guard'
 
 const routes: Routes = [
   { path: "welcome", component: WelcomeComponent },
   { path: "products", component: ProductsListComponent },
-  { path: "products/:id", component: ProductDetailsComponent },
+  { path: "products/:id", canActivate: [ProductDetailGuard], component: ProductDetailsComponent },
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
   { path: "**", redirectTo: '' }
 ];
