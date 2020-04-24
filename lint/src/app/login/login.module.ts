311faab93a576bc0144638858d53fc363f6b0ae9
Ramaswamy Narayanan <ramaswamy.bn@perficient.com>
2020-04-24 12:17:07 +0530

    uadded logout in login module

--- lint/src/app/login/login.module.ts
+++ lint/src/app/login/login.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LogoutComponent } from './logout/logout.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LogoutComponent],
   imports: [
     CommonModule
   ]
