68508d884eaf95619b925261887827239ef862e9
Ramaswamy Narayanan <ramaswamy.bn@perficient.com>
2020-04-24 11:45:21 +0530

    g directive

--- lint/src/app/app.module.ts
+++ lint/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { OrdersComponent } from './orders/orders.component';
 import { Orders2Component } from './orders2/orders2.component';
+import { SearchBoxDirective } from './search-box.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     OrdersComponent,
-    Orders2Component
+    Orders2Component,
+    SearchBoxDirective
   ],
   imports: [
     BrowserModule
