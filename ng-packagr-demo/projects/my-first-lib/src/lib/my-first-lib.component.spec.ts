9a62c291f05a98852d962047a68e3a1170d10bbd
Ramaswamy Narayanan <ramaswamy.bn@perficient.com>
2020-04-30 18:00:25 +0530

    added the library files

---
+++ ng-packagr-demo/projects/my-first-lib/src/lib/my-first-lib.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MyFirstLibComponent } from './my-first-lib.component';
+
+describe('MyFirstLibComponent', () => {
+  let component: MyFirstLibComponent;
+  let fixture: ComponentFixture<MyFirstLibComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ MyFirstLibComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MyFirstLibComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
